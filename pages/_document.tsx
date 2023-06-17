8066a56ac9bfb07c44a4fd6e716c6870b48437da
mehdi faraji <mahdifarajideveloper@gmail.com>
2023-06-17 09:45:43 +0330

    removed extra content from document and fixed the redirect issue

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,30 +6,9 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="description" content="آزمایشگاه ژنتیک مندل" />
         <meta name="theme-color" content="#ffffff" />
-        <meta name="keywords" content="آزمایشگاه ژنتیک مندل" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
-        <link
-          href="/icons/favicon-16x16.png"
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-        />
-        <link
-          href="/icons/favicon-32x32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-        />
-        <link
-          href="/icons/favicon-96x96.png"
-          rel="icon"
-          type="image/png"
-          sizes="96x96"
-        />
-        <link rel="apple-touch-icon" href="/apple-icon.png" />
       </Head>
       <body>
         <Main />
